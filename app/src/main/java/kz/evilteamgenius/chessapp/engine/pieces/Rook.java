9ce7ce0170f26bf0bbd19317365a19c51b9ab9c6
edil <edilserjan@gmail.com>
2019-10-19 12:24:42 +0600

    implemented abstarct methods temporally for dismissing the errors

--- app/src/main/java/kz/evilteamgenius/chessapp/engine/pieces/Rook.java
+++ app/src/main/java/kz/evilteamgenius/chessapp/engine/pieces/Rook.java
@@ -1,4 +1,28 @@
 package kz.evilteamgenius.chessapp.engine.pieces;
 
+import java.util.Collection;
+
+import kz.evilteamgenius.chessapp.engine.Alliance;
+import kz.evilteamgenius.chessapp.engine.Move;
+import kz.evilteamgenius.chessapp.engine.board.Board;
+
 public final class Rook extends Piece {
+    Rook(PieceType type, Alliance alliance, int piecePosition, boolean isFirstMove) {
+        super(type, alliance, piecePosition, isFirstMove);
+    }
+
+    @Override
+    public int locationBonus() {
+        return 0;
+    }
+
+    @Override
+    public Piece movePiece(Move move) {
+        return null;
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        return null;
+    }
 }
