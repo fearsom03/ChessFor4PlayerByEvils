c015a64ac5e304ab4a822cc6313c7f029e7866a2
edil <edilserjan@gmail.com>
2019-12-13 19:50:27 +0600

    Login function

--- app/src/main/java/kz/evilteamgenius/chessapp/activity/LoginActivity.java
+++ app/src/main/java/kz/evilteamgenius/chessapp/activity/LoginActivity.java
@@ -1,18 +1,27 @@
 package kz.evilteamgenius.chessapp.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import kz.evilteamgenius.chessapp.R;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,7 +30,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     MaterialButton signInButton;
     @BindView(R.id.createAccTextView)
     TextView createAccount;
+    @BindView(R.id.username)
+    TextInputEditText usernameEditText;
+    @BindView(R.id.password)
+    TextInputEditText passwordEditText;
 
+    final String url_Login = "https://k-chess.herokuapp.com/api/auth/token";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,13 +55,67 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Intent intent;
         switch (view.getId()) {
             case R.id.signInButton:
-                intent = new Intent(this, MainAppPage.class);
-                startActivity(intent);
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                new LoginUser().execute(username, password);
+
+//                intent = new Intent(this, MainAppPage.class);
+//                startActivity(intent);
                 break;
             case R.id.createAccTextView:
                 intent = new Intent(this, RegistrationActivity.class);
                 startActivity(intent);
                 break;
         }
+    }
+
+    public class LoginUser extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String username = strings[0];
+            String password = strings[1];
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody formBody = new FormBody.Builder()
+                    .add("username", username)
+                    .add("password", password)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url_Login)
+                    .post(formBody)
+                    .build();
+
+            Response response = null;
+            try{
+                response = okHttpClient.newCall(request).execute();
+                if(response.isSuccessful()){
+                    String JWTtoken = response.body().string();
+                    if(!JWTtoken.isEmpty()){
+                        showToast(JWTtoken);
+                        Intent i = new Intent(LoginActivity.this,
+                                MainAppPage.class);
+                        startActivity(i);
+                        finish();
+                    }else{
+                        showToast("Wrong Email or Password!");
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public void showToast(final String Text){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(LoginActivity.this,
+                        Text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
