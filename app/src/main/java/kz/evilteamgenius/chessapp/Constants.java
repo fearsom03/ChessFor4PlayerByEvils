970970c9a6e0ba490656a72820f921ba5e7263c0
edil <edilserjan@gmail.com>
2020-05-04 00:46:22 +0600

    server urls changes

--- app/src/main/java/kz/evilteamgenius/chessapp/Constants.java
+++ app/src/main/java/kz/evilteamgenius/chessapp/Constants.java
@@ -2,16 +2,15 @@ package kz.evilteamgenius.chessapp;
 
 public class Constants {
 
-    //   public static final String Url_BASE = "https://k-chess.herokuapp.com";
-    //   public static final String UrlDefault ="https://k-chess.herokuapp.com/";
-    //   public static final String URL_GET_LATEST_MOVE2P = "https://k-chess.herokuapp.com/api/game/get/move";
-    //   public static final String URL_MAKE_MOVE_GAME2P = "https://k-chess.herokuapp.com/api/game/make/move";
+       public static final String Url_BASE = "https://k-chess.herokuapp.com";
+       public static final String UrlDefault ="https://k-chess.herokuapp.com/";
+       public static final String URL_MAKE_MOVE_GAME2P = "https://k-chess.herokuapp.com/api/game2p/make/move";
 
-    public static final String Url_BASE = "http://192.168.0.103:5000";
-    public static final String UrlDefault ="http://192.168.0.103:5000/";
-    public static final String URL_GET_LATEST_MOVE2P = "http://192.168.0.103:5000/api/game2p/get/move";
-    public static final String URL_MAKE_MOVE_GAME2P = "http://192.168.0.103:5000/api/game2p/make/move";
+//    public static final String Url_BASE = "http://192.168.0.103:5000";
+//    public static final String UrlDefault ="http://192.168.0.103:5000/";
+//    public static final String URL_MAKE_MOVE_GAME2P = "http://192.168.0.103:5000/api/game2p/make/move";
 
+    public static final String URL_GET_LATEST_MOVE2P = "https://k-chess.herokuapp.com/api/game/get/move";
     public static final String UrlForLogin ="https://k-chess.herokuapp.com/api/auth/token";
     public static final String Url_RegisterSubmit = "https://k-chess.herokuapp.com/api/registration/submit";
     public static final String url_IsUserNameAvailable = "https://k-chess.herokuapp.com/api/registration/isUsernameAvailable";
